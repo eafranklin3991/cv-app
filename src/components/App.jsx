748dec886c2b9783dc46d0bb8b3ea3ef1a09fa8c
eafranklin3991 <eafranklin3991@gmail.com>
2026-03-30 14:31:50 -0400

    Initial attempt at submit/edit mode switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import '../styles/App.css'
 import General from './General.jsx'
 import Education from './Education.jsx'
 import Practical from './Practical.jsx'
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,11 +1,40 @@
+import { useState } from 'react'
+
 function General() {
+    const stateValues = ["locked", "edit"];
+
+    const [stateValue, setStateValue] = useState("locked");
+
+    const onEditClick = (state) => () => {
+        setStateValue(state);
+
+        const contentContainer = document.querySelector(".content");
+
+        contentContainer.innerHTML = state === "locked" ? form : "";
+
+        stateValue = "edit";
+    };
+
+    const form = <>
+        <form action="">
+            <label htmlFor="name">Name: </label><br />
+            <input type="text" name="name" id="name" /><br />
+
+            <label htmlFor="email">Email: </label><br />
+            <input type="email" name="email" id="email" /><br />
+
+            <label htmlFor="phone">Phone: </label><br />
+            <input type="tel" name="phone" id="phone" /><br />
+
+            <input type="submit" value="Submit" />
+        </form>
+    </>
+
     return (
         <div>
-            <h1>Name</h1>
-            <h1>Email</h1>
-            <h1>Phone</h1>
-            <button>Edit</button>
-            <button>Submit</button>
+            <h1>General</h1>
+            <div className="content">Hi</div>
+            <button onClick={onEditClick(stateValue)}>Edit</button>
         </div>
     );
 }
